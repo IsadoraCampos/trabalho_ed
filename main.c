21e36718a69539261f7a393621a43c3af172397c
MariaRampanelli <123091950+MariaRampanelli@users.noreply.github.com>
2024-06-23 18:16:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 
 int main(){
     
-    int opcao;
-    Lista *l;
-    initLista(l);
-
+    int opcao, id, tempo;
+    Node* root = NULL;
+    
     while (1){
         printf("\n----- Menu de Opções -----\n");
         printf(" 1 - Adicionar tarefa\n");
@@ -18,7 +17,13 @@ int main(){
         printf("Digite a sua opção: \n");
         scanf("%d", &opcao);
         if (opcao == 1){
-            l = adicionar(l);
+           printf("Insira o ID da tarefa: \n");
+           scanf("%d", &id);
+           printf("Insira o tempo(em horas) para a realização da tarefa\n");
+           scanf("%d", &tempo);
+           printf("Insira a descrição\n");
+           scanf("%d", &descricao);
+           insertTree(root, criarNo(id, descricao, tempo, Ativo));
         }
         if (opcao == 2){
             vizualizar(l);
