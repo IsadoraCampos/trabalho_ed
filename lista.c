00360f4dcd81c25ed50405dcaf5c79f2aa63aff4
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-06-25 10:56:08 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -12,7 +12,7 @@ struct node{
 
 typedef struct node Node;
 
-Node* criarNo(int id, char* descricao, int tempo, char* situacao){
+Node* criarNo(int id, char* descricao, int tempo, char* situacao){  // função que cria o nó
     Node *newNode= (Node *)malloc(sizeof(Node));
     newNode->id = id;
     strcpy(newNode->descricao, descricao);
@@ -22,7 +22,7 @@ Node* criarNo(int id, char* descricao, int tempo, char* situacao){
     return newNode;
 }
 
-Node* insertTree(Node* node, Node* newNode){
+Node* insertTree(Node* node, Node* newNode){     // função para inserir na árvore
     if(node == NULL){
         return newNode;
     }
@@ -34,7 +34,36 @@ Node* insertTree(Node* node, Node* newNode){
     return node;
 }
 
-void visualizar(Node *root){
-    
+Node* ordTempo(Node *node, Node* newNode){   // função que ordena pelo tempo para a visualização das tarefas
+    if (node == NULL){
+        return newNode;
+    }
+    if(newNode->tempo < node->tempo){
+        node->left = ordTempo(node->left, newNode);
+    } else{
+        node->right = ordTempo(node->right, newNode);
+    }
+    return node;
+}
+
+void visualizar(Node *root, int n){
+    if (root != NULL){                  // método de busca In-ordem
+        visualizar(root->left,n);
+        if (n == 1){
+            if (strcmp(root->situacao, "Ativo") == 0){
+                printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
+            }
+        }
+        if (n == 2){
+            if (strcmp(root->situacao, "Concluída") == 0){
+                printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
+            }
+        }
+        if (n == 3){
+            printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
+        }
+
+        visualizar(root->right,n);
+    }
 }
 
