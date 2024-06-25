67b58bb37afd9226bd4b712460937dfe41fb3f55
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-06-25 10:54:49 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -13,6 +13,7 @@ typedef struct node Node;
 
 Node* criarNo(int id, char* descricao, int tempo, char* situacao);
 Node* insertTree(Node* node, Node* newNode);
-void visualisar(Node *root);
+Node* ordTempo(Node *node, Node* newNode);
+void visualizar(Node *root, int n);
 
 #endif
