87b81c0a1427223ef0f5691c70228a054385a8f9
IsadoraCampos <149686078+IsadoraCampos@users.noreply.github.com>
2024-06-26 17:39:37 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -5,7 +5,7 @@ struct node {
     int id;
     char descricao[30];
     int tempo;
-    char situacao[10];
+    char situacao[20];
     struct lista *next;
 };
 
@@ -15,6 +15,7 @@ Node* criarNo(int id, char* descricao, int tempo, char* situacao);
 Node* insertTree(Node* node, Node* newNode);
 Node* ordTempo(Node *node, Node* newNode);
 void visualizar(Node *root, int n);
-Node* concluir(Node *root, int n);
+int concluir(Node *root, int n);
+int atualizaRoot1(Node *root,Node *root1,int n);
 
 #endif
