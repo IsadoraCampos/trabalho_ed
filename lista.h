a127df7714bf88cd95646cd8bd82c0a30259c2f7
MariaRampanelli <123091950+MariaRampanelli@users.noreply.github.com>
2024-06-30 20:49:09 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -15,7 +15,14 @@ Node* criarNo(int id, char* descricao, int tempo, char* situacao);
 Node* insertTree(Node* node, Node* newNode);
 Node* ordTempo(Node *node, Node* newNode);
 void visualizar(Node *root, int n);
+void visualizarConcluidas(Node *root);
+void visualizarTarefas(Node *root, int somenteAtivas);
 int concluir(Node *root, int n);
 int atualizaRoot1(Node *root,Node *root1,int n);
+Node* deleteNode(Node* root, int id,  int* taskDeleted);
+Node* minValueNode(Node* node);
+int idExiste(Node *root, int id);
+int gerarID(Node *root);
+
 
 #endif
