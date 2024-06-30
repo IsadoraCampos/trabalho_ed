4e13720b2d6e3da4039f28b8f1f6dff1eac856f0
MariaRampanelli <123091950+MariaRampanelli@users.noreply.github.com>
2024-06-30 20:48:34 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct node{
     int id;
@@ -20,6 +21,31 @@ Node* criarNo(int id, char* descricao, int tempo, char* situacao){  // função 
     strcpy(newNode->situacao, situacao);
     newNode->left = newNode->right = NULL;
     return newNode;
+}
+
+//verifica se existe na árvore
+int idExiste(Node *root, int id) {
+    if (root == NULL) {
+        return false;
+    }
+    if (root->id == id) {
+        return true;
+    }
+    if (id < root->id) {
+        return idExiste(root->left, id);
+    } else {
+        return idExiste(root->right, id);
+    }
+}
+
+// gera um novo id e chama a função que verifica se existe
+int gerarID(Node *root) {
+    int novoID = rand() % 100; // Limitado até 50 conforme solicitado
+    if (!idExiste(root, novoID)) {
+        return novoID;
+    } else {
+        return gerarID(root);
+    }
 }
 
 Node* insertTree(Node* node, Node* newNode){     // função para inserir na árvore
@@ -46,24 +72,40 @@ Node* ordTempo(Node *node, Node* newNode){   // função que ordena pelo tempo p
     return node;
 }
 
-void visualizar(Node *root, int n){
-    if (root != NULL){                  // método de busca In-ordem
-        visualizar(root->left,n);
-        if (n == 1){
-            if (strcmp(root->situacao, "Ativo") == 0){
-                printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
-            }
-        }
-        if (n == 2){
-            if (strcmp(root->situacao, "Concluída") == 0){
-                printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
-            }
-        }
-        if (n == 3){
+void visualizarTarefas(Node *root, int somenteAtivas) {
+    if (root == NULL) {
+        return;
+    }
+    // Visualizar as tarefas ativas primeiro
+    if (somenteAtivas == 1 || somenteAtivas == 3) {
+        visualizarTarefas(root->left, somenteAtivas);
+        if (strcmp(root->situacao, "Ativo") == 0) {
             printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
         }
+        visualizarTarefas(root->right, somenteAtivas);
+    }
+}
 
-        visualizar(root->right,n);
+void visualizarConcluidas(Node *root) {
+    if (root == NULL) {
+        return;
+    }
+    // Visualizar somente as tarefas concluídas
+    visualizarConcluidas(root->left);
+    if (strcmp(root->situacao, "Concluída") == 0) {
+        printf("Tempo: %dh | ID: %d | Descrição: %s | Situação: %s\n", root->tempo, root->id, root->descricao, root->situacao);
+    }
+    visualizarConcluidas(root->right);
+}
+
+void visualizar(Node *root, int n) {
+    if (n == 1) {
+        visualizarTarefas(root, 1);
+    } else if (n == 2) {
+        visualizarConcluidas(root);
+    } else if (n == 3) {
+        visualizarTarefas(root, 3);
+        visualizarConcluidas(root);
     }
 }
 
@@ -106,14 +148,15 @@ Node* minValueNode(Node* node) {
     return current;
 }
 
-Node* deleteNode(Node* root, int id) { //deletar um elemento da árvore com busca de id
+Node* deleteNode(Node* root, int id,  int* taskDeleted) { //deletar um elemento da árvore com busca de id
     if (root == NULL) return root;
 
-    if (id < root->id)
-        root->left = deleteNode(root->left, id); // se o id for menor que a raiz ele vai para a esquerda e chama novamente a função
+     if (id < root->id)
+        root->left = deleteNode(root->left, id, taskDeleted); // se o id for menor que a raiz ele vai para a esquerda e chama novamente a função
     else if (id > root->id)
-        root->right = deleteNode(root->right, id); // se o id for maior que a raiz ele vai para a direita e chama novamente a função
+        root->right = deleteNode(root->right, id, taskDeleted); // se o id for maior que a raiz ele vai para a direita e chama novamente a função
     else {  // achamos o nó do id que queremos deletar
+        *taskDeleted = 1;
         if (root->left == NULL) { //verificando se na esquerda tem um nó, se não tiver apaga 
             Node* temp = root->right;
             free(root);
@@ -127,16 +170,14 @@ Node* deleteNode(Node* root, int id) { //deletar um elemento da árvore com busc
         // Caso o nó tenha dois filhos, ele vai pegar o proximo (menor na arvore à direita)
         Node* temp = minValueNode(root->right);
 
-        // Copiar o conteúdo do proximo in-order para este nó, ou seja, vai subistituir o nó que excluimos pelo próximo
+        // Copiar o conteúdo do proximo in-order para este nó, ou seja, vai substituir o nó que excluímos pelo próximo
         root->id = temp->id;
         strcpy(root->descricao, temp->descricao);
         root->tempo = temp->tempo;
         strcpy(root->situacao, temp->situacao);
 
         // Deletar o proximo in-order
-        root->right = deleteNode(root->right, temp->id);
+        root->right = deleteNode(root->right, temp->id, taskDeleted);
     }
     return root;
 }
-
-
